fe1b097eb367ec7228adc3a793ef2bbdad0e470a
emanuel-xavier <emanueljx@outlook.com>
2025-05-21 22:06:05 -0300

    first commit

---
+++ src/main/java/dev/emanuelxavier/JavaSprintAPI/JavaSprintApiApplication.java
@@ -0,0 +1,13 @@
+package dev.emanuelxavier.JavaSprintAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSprintApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSprintApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/emanuelxavier/JavaSprintAPI/JavaSprintApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emanuelxavier.JavaSprintAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSprintApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
