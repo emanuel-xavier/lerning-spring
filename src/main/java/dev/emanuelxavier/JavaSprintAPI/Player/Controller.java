ea3791ab0ef190773991f7c90dc081781007bc00
emanuel-xavier <emanueljx@outlook.com>
2025-05-28 22:27:34 -0300

    feat: list all players

--- src/main/java/dev/emanuelxavier/JavaSprintAPI/Player/Controller.java
+++ src/main/java/dev/emanuelxavier/JavaSprintAPI/Player/Controller.java
@@ -1,5 +1,7 @@
 package dev.emanuelxavier.JavaSprintAPI.Player;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,14 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/player")
 public class Controller {
 
+  private PlayerService playserService;
+
+  public Controller(PlayerService playserService) {
+    this.playserService = playserService;
+  }
+
   @PostMapping("/create")
   public String create() {
     return "";
   }
 
   @GetMapping("/list")
-  public String list() {
-    return "";
+  public List<PlayerModel> list() {
+    return playserService.list();
   }
 
   @GetMapping("/player")
