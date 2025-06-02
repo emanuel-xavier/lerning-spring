ba9d686276e1377f2578ab8489478dea1969593b
emanuel-xavier <emanueljx@outlook.com>
2025-06-01 23:41:50 -0300

    feat: implements a functional mission controllers

--- src/main/java/dev/emanuelxavier/JavaSprintAPI/Mission/MissionController.java
+++ src/main/java/dev/emanuelxavier/JavaSprintAPI/Mission/MissionController.java
@@ -1,38 +1,77 @@
 package dev.emanuelxavier.JavaSprintAPI.Mission;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/mission")
 public class MissionController {
+  private MissionService missionService;
 
-  @GetMapping("/list")
-  public String list() {
-    return "";
+  public MissionController(MissionService missionService) {
+    this.missionService = missionService;
   }
 
-  @GetMapping("/mission")
-  public String searchById() {
-    return "";
+  @GetMapping("/list")
+  public ResponseEntity<List<MissionDTO>> list() {
+    List<MissionDTO> missions = missionService.list();
+
+    return ResponseEntity.ok(missions);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> searchById(@PathVariable Long id) {
+    MissionDTO mission = missionService.findByID(id);
+
+    if (mission == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+    }
+
+    return ResponseEntity.ok(mission);
   }
 
   @PostMapping("/create")
-  public String create() {
-    return "";
+  public ResponseEntity<?> create(@RequestBody MissionDTO mission) {
+    MissionDTO createdMission = missionService.create(mission);
+
+    if (createdMission == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Could not create the mission");
+    }
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdMission);
   }
 
-  @DeleteMapping("/delete")
-  public String delete() {
-    return "";
+  @PutMapping("/{id}")
+  public ResponseEntity<?> update(@PathVariable Long id, @RequestBody MissionDTO newPlayer) {
+    MissionDTO mission = missionService.update(id, newPlayer);
+
+    if (mission == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+    }
+
+    return ResponseEntity.ok(mission);
   }
 
-  @PutMapping("/update")
-  public String update() {
-    return "";
+  @DeleteMapping("/{id}")
+  public ResponseEntity<String> deleteById(@PathVariable Long id) {
+    if (missionService.findByID(id) == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body("mission with id " + id + " not found");
+    }
+
+    missionService.deleteById(id);
+    return ResponseEntity.ok("mission with id " + id + " was deleted successfully");
+
   }
+
 }
